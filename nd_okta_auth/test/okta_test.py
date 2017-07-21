222c8f4ebc04399d7878bef25d92e24014d7f1ba
Matt Wise <matt@nextdoor.com>
2017-07-21 16:38:44 -0700

    Refactor the okta_test package

--- nd_okta_auth/test/okta_test.py
+++ nd_okta_auth/test/okta_test.py
@@ -6,9 +6,6 @@ from nd_okta_auth import okta
 
 class OktaTest(unittest.TestCase):
 
-    def setUp(self):
-        self.client = okta.Okta('server', 'username', 'password')
-
     def test_init_blank_inputs(self):
         with self.assertRaises(okta.EmptyInput):
             okta.Okta(server='', username='test', password='test')
@@ -17,6 +14,9 @@ class OktaTest(unittest.TestCase):
             okta.Okta(server=None, username='test', password='test')
 
     def test_request_good_response(self):
+        client = okta.Okta('server', 'username', 'password')
+        client.session = mock.MagicMock(name='session')
+
         # Ultimately this is the dict we want to get back
         expected_dict = {'ok': True}
 
@@ -25,21 +25,24 @@ class OktaTest(unittest.TestCase):
         fake_response_object = mock.MagicMock(name='response')
         fake_response_object.json.return_value = expected_dict
 
-        with mock.patch('requests.post') as m:
-            m.return_value = fake_response_object
-            ret = self.client._request('/test', {'test': True})
+        client.session.post.return_value = fake_response_object
+        ret = client._request('/test', {'test': True})
 
         # Validate that the call went out as expected, with the supplied input
-        m.assert_called_with(
+        client.session.post.assert_called_with(
             headers={'Content-Type': 'application/json',
                      'Accept': 'application/json'},
             json={'test': True},
-            url='https://server/api/v1/test')
+            url='https://server/api/v1/test',
+            allow_redirects=False)
 
         # Validate that we got back the expected_dict
         self.assertEquals(ret, expected_dict)
 
     def test_request_with_full_url(self):
+        client = okta.Okta('server', 'username', 'password')
+        client.session = mock.MagicMock(name='session')
+
         # Ultimately this is the dict we want to get back
         expected_dict = {'ok': True}
 
@@ -48,21 +51,23 @@ class OktaTest(unittest.TestCase):
         fake_response_object = mock.MagicMock(name='response')
         fake_response_object.json.return_value = expected_dict
 
-        with mock.patch('requests.post') as m:
-            m.return_value = fake_response_object
-            ret = self.client._request('http://test/test', {'test': True})
+        client.session.post.return_value = fake_response_object
+        ret = client._request('http://test/test', {'test': True})
 
         # Validate that the call went out as expected, with the supplied input
-        m.assert_called_with(
+        client.session.post.assert_called_with(
             headers={'Content-Type': 'application/json',
                      'Accept': 'application/json'},
             json={'test': True},
-            url='http://test/test')
+            url='http://test/test',
+            allow_redirects=False)
 
         # Validate that we got back the expected_dict
         self.assertEquals(ret, expected_dict)
 
     def test_request_bad_response(self):
+        client = okta.Okta('server', 'username', 'password')
+        client.session = mock.MagicMock(name='session')
 
         class TestExc(Exception):
             '''Test Exception'''
@@ -70,12 +75,13 @@ class OktaTest(unittest.TestCase):
         fake_response_object = mock.MagicMock(name='response')
         fake_response_object.raise_for_status.side_effect = TestExc()
 
-        with mock.patch('requests.post') as m:
-            m.return_value = fake_response_object
-            with self.assertRaises(TestExc):
-                self.client._request('/test', {'test': True})
+        client.session.post.return_value = fake_response_object
+        with self.assertRaises(TestExc):
+            client._request('/test', {'test': True})
 
     def test_set_token(self):
+        client = okta.Okta('server', 'username', 'password')
+        client.session = mock.MagicMock(name='session')
         input = {
             u'status': u'SUCCESS',
             u'expiresAt': u'2017-07-21T19:23:44.000Z',
@@ -93,5 +99,5 @@ class OktaTest(unittest.TestCase):
             },
             u'sessionToken': u'fake_token'}
 
-        self.client.set_token(input)
-        self.assertEquals(self.client.session_token, 'fake_token')
+        client.set_token(input)
+        self.assertEquals(client.session_token, 'fake_token')
